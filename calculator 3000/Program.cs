d903d2147c6e5cce4f98194a3ee2763f22fcdba0
Muhamed <147518615+Namaki66@users.noreply.github.com>
2023-10-10 16:05:11 +0200

    teste commit

---
+++ calculator 3000/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator_3000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1; int b = 2;
+            Console.WriteLine("le calculator 3000 ");
+
+            Console.WriteLine("-------------------");
+
+           a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("donnez moi un chiffre" + a);
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
